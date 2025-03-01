cdbe8291877a4e281992abb2ecfb06191678b1ee
alexviar <alexanderviar@gmail.com>
2025-03-01 15:26:01 -0400

    refactor: App

--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -1,21 +1,7 @@
-import { useEffect } from "react"
 import { ErrorBoundary } from "./ErrorBoundary"
-import { Outlet, useNavigate } from "react-router-dom"
+import { Outlet } from "react-router-dom"
 
 export const AppContainer = () => {
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    const enrolled = localStorage.getItem('enrolled') ?? false
-    if (!enrolled) {
-      navigate('/enrollment', { replace: true })
-      return
-    }
-    const locked = localStorage.getItem('locked') ?? false
-    if (locked) {
-      navigate('/lockscreen', { replace: true })
-    }
-  }, [])
 
   return <ErrorBoundary>
     <Outlet />
--- src/setup/componets/App.tsx
+++ src/setup/componets/App.tsx
@@ -1,42 +1,97 @@
 import { Provider } from "react-redux"
-import LockScreen from "./features/lockscreen/components/LockScreen"
-import PurchasesList from "./features/purchases/components/PurchasesList"
-import { store } from "./store"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import { UnderConstruction } from "./UnderConstruction"
-import { ErrorBoundary } from "./ErrorBoundary"
-import { EnrollDevice } from "./features/devices/components/EnrollDevice"
-import { AppContainer } from "./AppContainer"
+import { ErrorBoundary } from "../../ErrorBoundary"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import { AppContainer } from "../../AppContainer"
+import { useEffect, useReducer } from "react"
+import { store } from "../store"
+import PurchasesList from "../../features/purchases/components/PurchasesList"
+import { DeviceEnrollmentScreen } from "../../features/devices/components/DeviceEnrollmentScreen"
+import LockScreen from "../../features/lockscreen/components/LockScreen"
+import '../nativeListener'
+import { Dashboard } from "../../features/dashboard/components/Dashboard"
 
-const router = createBrowserRouter([
-  {
-    path: '',
-    element: <AppContainer />,
-    children: [
-      {
-        path: '',
-        element: <UnderConstruction />
-      },
-      {
-        path: 'lockscreen',
-        element: <LockScreen />
-      },
-      {
-        path: 'enrollment',
-        element: <EnrollDevice />
-      },
-    ]
-  },
-  {
-    path: 'purchases',
-    element: <PurchasesList />
-  },
-])
+
+interface DeviceState {
+  enrollmentData: {
+    deviceId: number
+  } | null;
+  locked: boolean;
+}
+
+type Command =
+  | { type: 'setState'; payload: DeviceState }
+  | { type: 'lock' }
+  | {
+    type: 'finish_device_enrollment'; payload: {
+      deviceId: number
+      token?: string
+    }
+  };
+
+function deviceReducer(state: DeviceState | null, action: Command): DeviceState | null {
+  switch (action.type) {
+    case 'setState':
+      return action.payload;
+    case 'lock':
+      return { ...(state as DeviceState), locked: true };
+    case 'finish_device_enrollment':
+      return {
+        ...(state as DeviceState),
+        enrollmentData: {
+          deviceId: action.payload.deviceId
+        }
+      };
+    default:
+      return state;
+  }
+}
+
 
 export const App = () => {
+  const [deviceState, dispatch] = useReducer(deviceReducer, null)
+  console.log("DeviceState", deviceState)
+
+  useEffect(() => {
+    const remove = window.addNativeCommandHandler?.((command) => {
+      dispatch(command as Command)
+    })
+
+    return remove;
+  }, [])
+
+  useEffect(() => {
+    window.ReactNativeWebView?.postMessage(JSON.stringify({ event: 'getState' }))
+  }, [])
+
+  if (window.ReactNativeWebView && deviceState == null) {
+    return
+  }
+
+  function renderRoutes() {
+    if (window.ReactNativeWebView) {
+      if (deviceState!.locked) {
+        return <Route index element={<LockScreen />} />
+      }
+
+      if (!deviceState!.enrollmentData) {
+        return <Route index element={<DeviceEnrollmentScreen />} />
+      }
+      return <Route index element={<Dashboard />} />
+    }
+
+    return <Route element={<AppContainer />}>
+      <Route index element={<Navigate to="purchases" />} />
+      <Route path="purchases" element={<PurchasesList />} />
+    </Route>
+  }
+
   return <Provider store={store}>
     <ErrorBoundary>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <Routes>
+          {renderRoutes()}
+        </Routes>
+      </BrowserRouter>
     </ErrorBoundary>
   </Provider>
 }
